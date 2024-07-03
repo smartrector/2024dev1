f83e2b7260e7ccab672712698d495485045db9a8
smartrector <smartrector@gmail.com>
2024-07-03 15:00:01 +0900

    response

--- 13_TestApi/src/main/java/com/study/spring/api/TestEntityApi.java
+++ 13_TestApi/src/main/java/com/study/spring/api/TestEntityApi.java
@@ -1,11 +1,17 @@
 package com.study.spring.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.spring.service.TestService;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @RestController
 @AllArgsConstructor
@@ -13,8 +19,22 @@ public class TestEntityApi {
 	
 	private final TestService testService;
 	
-	@GetMapping("/test/entity/create")
-	public void  createTestEntity() {
-		 testService.create("hanyong5", 100);
+	@PostMapping("/test/entity/create")
+	public ResponseEntity<String> createTestEntity(
+			@RequestBody CreateTestEntityRequest request
+			) {
+		 
+		testService.create(request.getName(),request.getAge());
+		
+		return new ResponseEntity<>("{\"ok\"}",HttpStatus.CREATED);
 	}
+	
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class CreateTestEntityRequest{
+		private String name;
+		private int age;
+	}
+	
 }
